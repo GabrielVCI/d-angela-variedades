b51de27cbfa89034e9de536e09e53a21fc1714a3
Gabriel Valdez <GabrielVCI>
2024-08-30 14:22:27 -0400

    FT-01-AppDbContext
    He realizado la configuración básica del AppDbContext

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,6 +1,28 @@
-﻿namespace d_angela_variedades
+﻿using d_angela_variedades.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace d_angela_variedades
 {
-    public class ApplicationDbCOntext
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions dbContext) : base (dbContext)
+        {
+            
+        }
+
+        DbSet<Categoria> Categorias { get; set; }   
+        DbSet<Subcategoria> Subategorias { get; set; }   
+        DbSet<Ventas> Ventas { get; set; }   
+        DbSet<Productos> Productos{ get; set; }   
+        DbSet<Empresas> Empresas{ get; set; }   
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
     }
+
+    
 }
